f8913f099f52e33d001a7f532478980ccbf53dfa
Brian Paner <brian.paner@omega-kr.com>
2026-01-20 14:27:12 -0800

    updated run.py

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
-# run.py
 from waitress import serve
-from app import app  # your Flask app instance
+from app import app  # your Flask instance
 
 if __name__ == "__main__":
-    serve(app, host="0.0.0.0", port=8000)
+    # Use $PORT if running on Render, otherwise default to 8000 for local dev
+    import os
+    port = int(os.environ.get("PORT", 8000))
+    serve(app, host="0.0.0.0", port=port)
